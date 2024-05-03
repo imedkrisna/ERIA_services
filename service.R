6c26d0da316d7dabf61f3186917d09e54fca3e78
Krisna Gupta <44009808+imedkrisna@users.noreply.github.com>
2024-05-04 06:30:57 +0700

    wew

--- service.R
+++ service.R
@@ -8,8 +8,9 @@ srv<-c("IDN_D","IDN_E","IDN_F","IDN_G","IDN_H49","IDN_H50",
        "IDN_J61","IDN_J62_63","IDN_K","IDN_L","IDN_M","IDN_N",
        "IDN_O","IDN_P","IDN_Q","IDN_R","IDN_S")
 
-i2002<-read_csv("2002.csv") |>filter(`...1` %in% srv ) |> arrange(desc(IDN_D)) |>
-  pivot_longer(!`...1`,names_to = "tujuan",values_to = "value") |> arrange(desc(value))
+i2002<-read_csv("2002.csv") |>filter(`...1` %in% srv ) |> 
+  pivot_longer(!`...1`,names_to = "tujuan",values_to = "value") |>
+  pivot_wider(names_from = `...1`,values_from = value)
 write_xlsx(i2002,"servicesoutput.xlsx")
 
 i2002<-read_csv("2002.csv") |> select(`...1`,srv) |> arrange(desc(IDN_D))
